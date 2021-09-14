49d033f45904fa89fa243b40bbf190eb3cf9fe06
mingo <mingo.wang@gllue.com>
2021-09-14 22:52:27 +0800

    解析 ts tsx

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const Koa = require('koa')
 const fs = require('fs')
+const path = require('path')
 const app = new Koa()
+const { transformSync } = require('@babel/core')
 
 app.use(async (ctx) => {
   const { url } = ctx.request
@@ -10,6 +12,46 @@ app.use(async (ctx) => {
     const body = fs.readFileSync('./index.html')
     ctx.type = 'text/html'
     ctx.body = body
+  } else if (url.endsWith('.tsx') || url.endsWith('.ts')) {
+    const p = path.join(__dirname, url)
+
+    const code = fs.readFileSync(p, 'utf8')
+
+    const parserPlugins = [
+      'jsx',
+      'importMeta',
+      // since the plugin now applies before esbuild transforms the code,
+      // we need to enable some stage 3 syntax since they are supported in
+      // TS and some environments already.
+      'topLevelAwait',
+      'classProperties',
+      'classPrivateProperties',
+      'classPrivateMethods',
+      'typescript',
+      'decorators-legacy',
+    ]
+
+    const result = transformSync(code, {
+      babelrc: false,
+      configFile: false,
+      // filename: id,
+      parserOpts: {
+        sourceType: 'module',
+        allowAwaitOutsideFunction: true,
+        plugins: parserPlugins,
+      },
+      generatorOpts: {
+        decoratorsBeforeExport: true,
+      },
+      plugins: [
+        require('@babel/plugin-transform-react-jsx'),
+        // require('@babel/plugin-transform-react-jsx-self'),
+        // require('@babel/plugin-transform-react-jsx-source'),
+      ],
+      sourceMaps: true,
+    })
+    ctx.type = 'application/javascript'
+    ctx.body = result.code;
   }
 })
 
